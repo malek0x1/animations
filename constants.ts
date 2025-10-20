07bce89053007015dbb6c016d14b6a08de4493d5
malek0x1 <malek0x1@gmail.com>
2025-10-20 17:08:55 +0400

    changes

--- constants.ts
+++ constants.ts
@@ -1,6 +1,13 @@
+export const STROKE_WIDTH = 2
 export const IMAGES=[
-    "https://github.com/ashishsharma-prog/Frontend-Assignment/blob/31069b184138c6a4524723a0f1a86d8981befd83/src/assets/hslider-4.png?raw=true",
-    "https://github.com/ashishsharma-prog/Frontend-Assignment/blob/31069b184138c6a4524723a0f1a86d8981befd83/src/assets/hslider-2.png?raw=true",
-    "https://github.com/ashishsharma-prog/Frontend-Assignment/blob/31069b184138c6a4524723a0f1a86d8981befd83/src/assets/slider2.webp?raw=true",
-    "https://github.com/ashishsharma-prog/Frontend-Assignment/blob/31069b184138c6a4524723a0f1a86d8981befd83/src/assets/slider4.webp?raw=true"
+    "https://images.pexels.com/photos/1631677/pexels-photo-1631677.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/2356059/pexels-photo-2356059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/259962/pexels-photo-259962.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 ]
+
+
+export const SIZE = 100
+export const RADIUS = (SIZE - STROKE_WIDTH) / 2
+export const CIRCUMFERENCE = 2 * Math.PI * RADIUS
+
